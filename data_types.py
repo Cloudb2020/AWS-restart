4c428ccebf5ef51a83a64079299f41ad05736907
Lori White <lori.r.white.24@gmail.com>
2021-08-10 16:29:55 -0400

    Created data_types.py

---
+++ data_types.py
@@ -0,0 +1,13 @@
+print("2 + 2 =", 2 + 2)
+print("4 - 2 =", 4 - 2)
+print("2 * 2 =", 2 * 2)
+print("4 / 2 =", 4 / 2)
+
+myVar = 1
+print(myVar, "is of data type", str(type(myVar)))
+myVar = 3.14
+print(myVar, "is of data type", str(type(myVar)))
+myVar = 5j
+print(myVar, "is of data type", str(type(myVar)))
+myVar = True
+print(myVar, "is of data type", str(type(myVar)))
