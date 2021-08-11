b76d19d56541fb5c7866cea102e1dd96ec2b4b22
Lori White <lori.r.white.24@gmail.com>
2021-08-11 15:20:28 -0400

    Updated loops.py

--- loops.py
+++ loops.py
@@ -12,15 +12,19 @@ number = random.randint(1,10)
 
 # Initializes the needed variables for the game.
 isGuessRight = False
+numberGuesses = 0
 
 # Runs the guessing game.
 while isGuessRight != True:
     guess = input("Guess a number between 1 and 10: ")
+    numberGuesses += 1
     if int(guess) == number:
-        print("You guessed {}. That is right! You win!".format(guess))
+        print("You guessed {} with {} tries. That is right! You win!".format(guess, numberGuesses))
         isGuessRight = True
+    elif int(guess) > number:
+        print("You guessed {}, which is higher then the actual number. Sorry, that isn’t it. Try again.".format(guess))
     else:
-        print("You guessed {}. Sorry, that isn’t it. Try again.".format(guess))
+        print("You guessed {}, which is lower then the actual number. Sorry, that isn’t it. Try again.".format(guess))
 
 # The for loop
 print("Let’s count to 10!")
