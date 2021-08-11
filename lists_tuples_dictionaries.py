a7b23e3002b9cf17974711b58fe0c5b994567837
Lori White <lori.r.white.24@gmail.com>
2021-08-11 10:57:25 -0400

    Created lists_tuples_dictionaries.py

---
+++ lists_tuples_dictionaries.py
@@ -0,0 +1,23 @@
+# Lists
+myFruitList= ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print("The Fruits: ", myFruitList[0], ",", myFruitList[1], ",", myFruitList[2])
+myFruitList[2] = "orange"
+print(myFruitList)
+# Tuples
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print("The Fruits: ", myFinalAnswerTuple[0], ",", myFinalAnswerTuple[1], ",", myFinalAnswerTuple[2])
+# Dictionaries
+myFavoriteFruitDictionary = {
+  "Adam" : "apple",
+  "Ben" : "banana",
+  "Penny" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Adam"])
+print(myFavoriteFruitDictionary["Ben"])
+print(myFavoriteFruitDictionary["Penny"])
